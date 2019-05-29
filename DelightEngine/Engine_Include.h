3bd09093986f788a6d1c0a9c814c761d257e5910
kimphore <topkpr1004@naver.com>
2019-05-29 21:02:37 +0900

    프로젝트 초기세팅
    세에에에팅

---
+++ DelightEngine/Engine_Include.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <xnamath.h>
+#include <assert.h>
+#include <DxErr.h>
+#include <windows.h>
+#include <dinput.h>
+
+#include <vector>
+#include <map>
+#include <list>
+#include <algorithm>
+#include <string.h>
+#include <chrono>
+#include <random>
+#include <ctime>
+#include <functional>
+#include <dinput.h>
+#include <utility>
---
+++ DelightEngine/Engine_TypeDef.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace Delight
+{
+	// Default 
+	typedef short				_short;
+	typedef int					_int;
+	typedef unsigned int		_uint;
+	typedef long				_long;
+
+	typedef float				_float;
+	typedef double				_double;
+
+	typedef unsigned char		_ubyte;
+	typedef char				_byte;
+	typedef char				_char;
+	typedef TCHAR				_tchar;
+	typedef bool				_bool;
+
+	typedef unsigned int		_result;
+
+}
